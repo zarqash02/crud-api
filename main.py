ee8a5eb1040416353cd5e0d66e92bf2c3c08f255
Zarqash <152395766+zarqash02@users.noreply.github.com>
2026-08-27 12:49:14 +0500

    root and health endpoints

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message" : "Hello World"}
+    return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+
+@app.get("/health")
+def isalive():
+    return {"status" : "ok"}
